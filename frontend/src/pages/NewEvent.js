901ebda73b29a4221c484c5ae8adc6a8d5a841d7
karimhafiz <hafiz_2010@outlook.com>
2024-05-20 17:49:19 +0100

    removed the auth loader

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,8 +1,8 @@
-import EventForm from '../components/EventForm';
+import EventForm from "../components/EventForm";
 
 function NewEventPage() {
+  console.log("Hello");
   return <EventForm method="post" />;
 }
 
 export default NewEventPage;
-
